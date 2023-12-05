53f86ef9e975e6e27bc6a9b211fca6b54804956e
Alain Riou <alain.riou14000@yahoo.com>
2023-12-06 00:14:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def random_subset(mask, n_elems):  # TODO: see if can be optimized
+    if not isinstance(mask, np.ndarray) or not mask.dtype == bool:
+        raise ValueError("The 'mask' parameter must be a boolean numpy array.")
+
+    if n_elems < 0 or n_elems > len(mask):
+        raise ValueError("Invalid value for 'n_elems' parameter.")
+
+    indices = np.arange(len(mask))[mask]
+
+    if n_elems > len(indices):
+        raise ValueError("The number of elements requested exceeds the number of elements in the mask.")
+
+    subset_indices = np.random.choice(indices, n_elems, replace=False)
+    subset = np.zeros(len(mask), dtype=bool)
+    subset[subset_indices] = True
+
+    return subset
+
+
+def generate_bms(k, m, n=None):
+    r""""""
+    # By default we generate a square (i.e. m = n)
+    n = n or m
+
+    # handle trivial cases
+    if k == 0:
+        return np.zeros((m, n), dtype=bool)
+
+    if k == n:
+        return np.ones((m, n), dtype=bool)
+
+    # the transpose of a BMS is also a BMS, so we eventually transpose for having m >= n
+    # so that we always iterate on the smallest dimension
+    transpose = m < n
+    if transpose:
+        m, n = n, m
+
+    q, r = divmod(m, n)
+    assert r == 0, "For non-trivial magic squares to exist, the number of rows and columns must divide the other."
+    km, kn = q*k, k
+
+    bms = np.zeros((m, n), dtype=bool)
+    s = np.zeros(m, dtype=int)
+
+    for t in range(n):
+        a1 = s == kn + t - n
+        a3 = s == kn
+        a2 = ~(a1 | a3)
+
+        to_check = a1 | random_subset(a2, km - a1.sum())
+
+        s[to_check] += 1
+        bms[:, t][to_check] = True
+
+    return bms.T if transpose else bms
+
+
+if __name__ == '__main__':
+    import sys
+
+    assert len(sys.argv) >= 3
+
+    k, m, n = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]) if len(sys.argv) > 3 else None
+
+    mat = generate_bms(k, m, n)
+    print(mat.astype(int))

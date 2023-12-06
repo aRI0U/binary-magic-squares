578b188d4b2114f6b7d6ef408271498a61e7e857
Alain Riou <alain.riou14000@yahoo.com>
2023-12-06 18:22:56 +0100

    parallel random subset generator

--- pytorch.py
+++ pytorch.py
@@ -3,18 +3,40 @@ from typing import Tuple
 import torch
 
 
-def random_subset(mask: torch.BoolTensor, n_elems: int) -> torch.BoolTensor:
-    indices = torch.where(mask)[0]
-    indices = indices[torch.randperm(len(indices))[:n_elems]]
-
-    subset = torch.zeros_like(mask)
-    subset[indices] = True
-
-    return subset
-
-
 def batch_random_subset(mask: torch.BoolTensor, n_elems: torch.LongTensor) -> torch.BoolTensor:
-    return torch.stack([random_subset(m, n) for m, n in zip(mask, n_elems)])
+    r"""Randomly creates a submask of the original one so that each row i has exactly n_elems[i] True elements.
+
+    Args:
+        mask (torch.BoolTensor): boolean mask, shape (batch_size, m)
+        n_elems (torch.LongTensor): number of elements to keep True per row
+
+    Returns:
+
+    """
+    m = mask.size(1)
+    sum_lines = mask.sum(dim=1, keepdim=True)
+    permutations = torch.randn_like(mask, dtype=torch.float32).argsort(dim=1)
+
+    valid_mask = permutations < sum_lines
+    valid_indices = permutations[valid_mask]
+
+    arange = torch.arange(m).unsqueeze(0)
+    to_keep_mask = arange < n_elems.unsqueeze(1)
+    to_keep_mask = to_keep_mask[arange < sum_lines]
+    to_keep_indices = valid_indices[to_keep_mask]
+
+    true_indices = torch.nonzero(mask, as_tuple=False)
+
+    sum_lines.squeeze_(1)
+    offset = torch.nn.functional.pad(sum_lines[:-1].cumsum(dim=0), (1, 0))
+    offset = offset.gather(0, true_indices[:, 0])[to_keep_mask]
+    true_indices = true_indices[to_keep_indices + offset]
+
+    final_mask = torch.zeros_like(mask)
+    # final_mask.scatter_(1, true_indices, 1)
+    final_mask[true_indices[:, 0], true_indices[:, 1]] = True
+
+    return final_mask
 
 
 def generate_bms(m: int,
@@ -64,5 +86,5 @@ if __name__ == '__main__':
 
     a, b, c = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]) if len(sys.argv) > 3 else None
 
-    mat, _ = generate_bms(b, c, a / c, 7)
+    mat, _ = generate_bms(b, c, a / c, 2)
     print(mat.long())
